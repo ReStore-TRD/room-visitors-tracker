32780a52b838952052b45ad2d15887a699e8131c
Dorsa74 <kabiridorsa@gmail.com>
2024-04-30 12:57:19 +0200

    test files

---
+++ visitor.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Define the function to perform the desired task
+def my_function(input_value):
+    result = input_value
+    return result
+
+# Create the Streamlit UI components
+st.title('ReStore Opening')
+input_value = st.text_input('Enter token number:')
+input_value2 = st.text_input('Which Room:')
+
+if st.button('Run'):
+    number = my_function(input_value)
+    room = my_function(input_value2)
+    st.write('Attended:', number)
