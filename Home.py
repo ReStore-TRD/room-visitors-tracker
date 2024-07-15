43c0c5bccc483179791afb6659a8c74a76220c02
anastasia <anastasia.kobzeva@ntnu.no>
2024-07-15 11:06:13 +0200

    Add home page

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://raw.githubusercontent.com/ReStore-TRD/room-visitors-tracker/main/images/ReStore_logo_resized.png);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+)
+
+st.header("Welcome to ReStore Opening!")
+
+st.write("Dear volunteer, thank you for your help at the opening!")
+st.write("Choose a room where you want to track visitors from the bar on the left.")
