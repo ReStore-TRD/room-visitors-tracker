b13df0aa3feb8756e4c2152b12cc76b272424e10
anastasia <anastasia.kobzeva@ntnu.no>
2024-08-09 21:44:28 +0200

    Increase visitor cap

--- pages/Sofas.py
+++ pages/Sofas.py
@@ -8,7 +8,7 @@ st.subheader("You are in the room: Sofas 🛋️")
 sheet = api_call()
 
 form = st.form(key='my_form')
-token = form.number_input(label='Enter visitor token', min_value=int(1), max_value=int(300))
+token = form.number_input(label='Enter visitor token', min_value=int(1), max_value=int(500))
 submitted = form.form_submit_button(label='Submit')
 if submitted:
     store_tokens(token, room, sheet)
